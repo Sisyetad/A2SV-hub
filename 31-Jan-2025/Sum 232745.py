6f3addafe4e2e556cf0144d9029ab80e139efe47
Sisay Thaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-01-31 23:54:45 +0300

    Created 'Sum' by A2SV Hub

---
+++ 31-Jan-2025/Sum 232745.py
@@ -0,0 +1,10 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+n = int(input())
+for i in range(n):
+    arr = list(map(int,input().split()))
+    arr.sort()
+    if arr[0] + arr[1] == arr[2]:
+        print("YES")
+    else:
+        print("NO")
