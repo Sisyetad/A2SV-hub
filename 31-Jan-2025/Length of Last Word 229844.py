5c7b15184d614b043ad1bc13fada5629cd477bbf
Sisay Thaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-01-31 10:04:41 +0300

    Created 'Length of Last Word' by A2SV Hub

---
+++ 31-Jan-2025/Length of Last Word 229844.py
@@ -0,0 +1,5 @@
+# Problem: Length of Last Word - https://leetcode.com/problems/length-of-last-word/description/
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.rstrip().split(" ")[-1])
