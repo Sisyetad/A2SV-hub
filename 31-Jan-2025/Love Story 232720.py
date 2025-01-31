b5b112d96fa8296ccb54f7f7b5545971d59a538a
Sisay Thaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-01-31 23:45:10 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 232720.py
@@ -0,0 +1,10 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+n = int(input())
+for i in range(n):
+    s = input()
+    count = 0
+    for i in range(len(s)):
+        if s[i] != "codeforces"[i]:
+            count += 1
+    print(count)
