4c642949f17bf933bef14a163e5e3794426f7faf
Sisay Thaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-02-06 11:25:11 +0300

    Created 'Masking Personal Information' by A2SV Hub

---
+++ 06-Feb-2025/Masking Personal Information 245967.py
@@ -0,0 +1,22 @@
+# Problem: Masking Personal Information - https://leetcode.com/problems/masking-personal-information/description/?envType=problem-list-v2&envId=string
+
+class Solution:
+    def maskPII(self, s: str) -> str:
+        s = s.lower()
+        arr = []
+        if s[-1].isalpha():
+            return s[0] + "*" * 5 + str(s[s.index('@')-1:])
+        else:
+            for i in s[-len(s):]:
+                if i.isalnum():
+                    arr.append(i)
+        arr = ['*'] * (len(arr)-4) + arr[-4:]
+        print(arr)
+        if len(arr) > 10:
+            arr.insert(0,'+')
+        arr.insert(-4,'-')
+        i = -8
+        while i > -len(arr) + 1:
+            arr.insert(i,'-')
+            i -= 4
+        return "".join(arr)
