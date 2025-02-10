1d87e5bf410f2abf1567336427f157f27cccca32
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-10 11:11:39 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 10-Feb-2025/D - Repeating Cipher 252956.py
@@ -0,0 +1,12 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n = int(input())
+t = input()
+ans=""
+i=0
+size = 1
+while i < n:
+    ans += t[i]
+    i += size
+    size += 1
+print(ans)
