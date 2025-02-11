aa27da1ad1042ab20c15dc0330a17fc0683c86c9
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-11 23:14:23 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 11-Feb-2025/Ehab Is an Odd Person 258293.py
@@ -0,0 +1,14 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input()) 
+nums = list(map(int,input().split()))
+even, odd = False, False
+for i in range(n):
+    if nums[i]%2:
+        odd = True
+    else:
+        even = True
+        
+if even and odd:
+    nums.sort()
+print(*nums)
