9c465f3573418cfb3a3e309bf291ed0e125aa124
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-17 22:44:34 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/E - Symmetrization 269790.py
@@ -0,0 +1,19 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+test = int(input())
+for i in range(test):
+    n = int(input())
+    matrix = []
+    for i in range(n):
+        row = list(input())
+        matrix.append(row)
+    operations = 0
+    for i in range((n + 1) // 2): 
+        for j in range(n // 2):
+            a = matrix[i][j]
+            b = matrix[j][n - i - 1]
+            c = matrix[n - i - 1][n - j - 1]
+            d = matrix[n - j - 1][i]
+            ones = int(a) + int(b) + int(c) + int(d)
+            operations += min(ones, 4 - ones)
+    print(operations)
