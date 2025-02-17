aa5656d2d4d05d9b30f62fa38b834a30b92f7490
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-17 22:45:08 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/A - Make the Product Zero 269797.py
@@ -0,0 +1,8 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+n = map(int,input().split())
+arr = list(map(int,input().split()))
+Min = abs(arr[0])
+for i in range(1,len(arr)):
+    Min = min(Min,abs(arr[i]))
+print(Min)
