8404cb33c029ab2072a9a7da8b96fccf8388234c
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-17 22:44:21 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/C - The Splitting Game 269788.py
@@ -0,0 +1,18 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+t = int(input())
+for i in range(t):
+    n = int(input())
+    s = [i for i in input()]
+    suffix = Counter(s)
+    prefix = set()
+    max_str = 0
+    for i in s:
+        prefix.add(i)
+        if suffix[i] > 1:
+            suffix[i] -= 1
+        else:
+            suffix.pop(i)
+        max_str = max(max_str,len(suffix) + len(prefix))
+    print(max_str)
