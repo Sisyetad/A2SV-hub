b460aee37b17d49fe83a4a151f976ee4b20e47ba
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-17 22:44:52 +0300

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/B - Special Matrix Quest 269796.py
@@ -0,0 +1,19 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+n = int(input())
+matrix = []
+for i in range(n):
+    matrix.append(list(map(int,input().split())))
+Sum = 0
+for f in range(n):
+    for j in range(n):
+        if f == j:
+            Sum += matrix[f][j]
+        if f == j:
+            Sum += matrix[f][-j-1]
+        if j == (n-1)//2:
+            Sum += matrix[f][j]
+        if f == (n-1)//2:
+            Sum += matrix[f][j]
+Sum -= (3*matrix[(n-1)//2][(n-1)//2])
+print(Sum)
