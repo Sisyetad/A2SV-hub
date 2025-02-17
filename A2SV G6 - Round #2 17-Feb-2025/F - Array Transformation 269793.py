ff5fcbac65b03117af6f26fbc7bd55e747211754
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-17 22:44:42 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/F - Array Transformation 269793.py
@@ -0,0 +1,13 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+test = int(input())
+for i in range(test):
+    n = int(input())
+    ans = n
+    arr = list(map(int,input().split()))
+    arr = Counter(arr)
+    arr = sorted(arr.items(), key = lambda x: x[1])
+    for i in range(len(arr)):
+        ans = min(ans,n-(len(arr)-i)*arr[i][1])
+    print(ans)
