5d6ca324259ee193009c7f450bfd9d0d958f553a
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-17 22:44:51 +0300

    Created 'D - Bomb Detection Validation' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/D - Bomb Detection Validation 269795.py
@@ -0,0 +1,28 @@
+# Problem: D - Bomb Detection Validation - https://codeforces.com/gym/586960/problem/D
+
+n, m = map(int,input().split())
+arr = []
+for i in range(n):
+    arr.append(input().split())
+flag = False
+for row in range(n):
+    for col in range(m):
+        Sum = 0
+        if arr[row][0][col] == '*':
+            continue
+        for i in range(row-1,row+2):
+            for j in range(col-1,col+2):
+                if 0 <= i < n and 0 <= j < m:
+                    if arr[i][0][j] == '*':
+                        Sum += 1
+        if (arr[row][0][col] != f'{Sum}'):
+            if (Sum == 0 and arr[row][0][col] == '.'):
+                continue
+            else:
+                print("NO")
+                flag = True
+                break
+    if flag:
+        break
+if not flag:
+    print("YES")
