253836c894144ea7c8ec9447d09b3756ada37b0f
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-18 16:44:54 +0300

    Created '3 Sum Closest' by A2SV Hub

---
+++ 18-Feb-2025/3 Sum Closest 271952.py
@@ -0,0 +1,21 @@
+# Problem: 3 Sum Closest - https://leetcode.com/problems/3sum-closest/description/
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        Sum = float('inf')
+        min_diff = float('inf')
+        for i in range(len(nums)):
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                if min_diff > abs(nums[right] + nums[left] + nums[i] - target):
+                    min_diff = abs(nums[right] + nums[left] + nums[i] - target)
+                    Sum = nums[right] + nums[left] + nums[i]
+                if nums[right] + nums[left] + nums[i] < target:
+                    left += 1
+                elif nums[right] + nums[left] + nums[i] > target:
+                    right -= 1
+                else:
+                    return nums[right] + nums[left] + nums[i]
+        return Sum
