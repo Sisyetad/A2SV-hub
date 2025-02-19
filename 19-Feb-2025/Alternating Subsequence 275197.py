3ba4e0b6c0e16ecf7981f0195f3163838fadfd38
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-19 23:26:25 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 19-Feb-2025/Alternating Subsequence 275197.py
@@ -0,0 +1,26 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+def alternating_subsequence(nums):
+    n = len(nums)
+    current_max = nums[0]
+    result_sum = 0
+    
+    for i in range(1, n):
+        if (nums[i] < 0 and current_max > 0) or (nums[i] > 0 and current_max < 0):
+            result_sum += current_max
+            current_max = nums[i]
+        else:
+            current_max = max(current_max, nums[i])
+    result_sum += current_max
+    return result_sum
+
+t = int(input())
+results = []
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    results.append(alternating_subsequence(nums))
+
+
+for res in results:
+    print(res)
