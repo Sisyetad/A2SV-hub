3b9f58e3a3eb20af7683e1f9d9985d05d4e6690e
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-20 10:19:33 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 20-Feb-2025/F - Binary Substrings with Exactly k Ones 275773.py
@@ -0,0 +1,24 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+s = input()
+from collections import defaultdict
+def numSubarraysWithSum(nums, goal):
+        prefix_count = defaultdict(int)  
+        prefix_count[0] = 1  
+        
+        prefix_sum = 0
+        result = 0
+        
+        for num in nums:
+            prefix_sum += int(num) 
+            
+            if (prefix_sum - goal) in prefix_count:
+                result += prefix_count[prefix_sum - goal]
+            
+            prefix_count[prefix_sum] += 1  
+        
+        return result
+
+ 
+print(numSubarraysWithSum(s,k))
