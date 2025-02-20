6e93e2bb54d4fda9c568426f6117a9be04f8f4db
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-20 10:19:56 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 20-Feb-2025/B - Anansi and Trip-Photographs 275780.py
@@ -0,0 +1,18 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+test = int(input())
+for i in range(test):
+    n, x = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    left, right = 0, n
+    flag = False
+    while (left < n) and (right < 2*n):
+        if arr[right] - arr[left] < x:
+            print("NO")
+            flag = True
+            break
+        right += 1
+        left += 1
+    if  not flag:
+        print("YES")
