3716b92ce0028406ae28c781c2f1bff2eb2f957b
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-24 21:51:58 +0300

    Created 'Books' by A2SV Hub

---
+++ 24-Feb-2025/Books 281938.py
@@ -0,0 +1,14 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+arr = list(map(int, input().split()))
+left = 0
+window = 0
+ans = 0
+for right in range(n):
+    window += arr[right]
+    while window > t:
+        window -= arr[left]
+        left += 1
+    ans = max(ans,right - left + 1)
+print(ans)
