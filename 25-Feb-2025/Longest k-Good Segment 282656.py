2bd53c4be6cee147bda3536f0e6fbe0c39108787
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-25 21:10:03 +0300

    Created 'Longest k-Good Segment' by A2SV Hub

---
+++ 25-Feb-2025/Longest k-Good Segment 282656.py
@@ -0,0 +1,19 @@
+# Problem: Longest k-Good Segment - https://codeforces.com/problemset/problem/616/D
+
+from collections import Counter
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+left = 0
+size = 0
+window = Counter()
+for right in range(n):
+    window[arr[right]] += 1
+    while len(window) > k:
+        window[arr[left]] -= 1
+        if window[arr[left]] == 0:
+            del window[arr[left]]
+        left += 1
+    if size < right - left + 1:
+        size = right - left + 1
+        ans = [left + 1, right + 1]
+print(*ans)
