c8ecf6a9090911526ad2ad78cadec4833a520a20
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-26 22:19:43 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/D - Minimize the Distance 285429.py
@@ -0,0 +1,12 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+arr = list(map(int, input().split()))
+total = sum(arr)
+arr.sort()
+if n % 2:
+    target = arr[n//2]
+else:
+    target = (arr[(n//2)-1])
+print(target)
+    
