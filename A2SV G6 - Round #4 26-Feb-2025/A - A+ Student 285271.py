3b5112cbc9800a7e32db9492b503b2deeb7a0fd4
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-26 20:59:55 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/A - A+ Student 285271.py
@@ -0,0 +1,13 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    
+    ans = [
+        max(0, max(b, c) - a + 1),
+        max(0, max(a, c) - b + 1),
+        max(0, max(a, b) - c + 1)
+    ]
+    
+    print(*ans)
