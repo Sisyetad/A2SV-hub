552643e5996ecab5f7b6beb176e30782060aa189
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-26 21:00:02 +0300

    Created 'B - Jo's Adventure' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/B - Jo's Adventure 285272.py
@@ -0,0 +1,27 @@
+# Problem: B - Jo's Adventure - https://codeforces.com/gym/590053/problem/B
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+p_diff = [0]*n
+s_diff = [0]*n
+for i in range(1,n):
+    if arr[i-1] > arr[i]:
+        p_diff[i] = arr[i-1] - arr[i]
+        
+    else:
+        p_diff[i] = 0
+for i in range(2,n+1):    
+    if arr[-i] < arr[-i+1]:
+        s_diff[-i] = arr[-i+1] - arr[-i]
+    else:
+        s_diff[-i] = 0
+
+for i in range(1,n):
+    p_diff[i] += p_diff[i-1]
+    s_diff[-i-1] += s_diff[-i]
+for q in range(m):
+    start, end = map(int, input().split())
+    if start < end:
+        print(p_diff[end-1] - p_diff[start-1])
+    else:
+        print(s_diff[end-1] - s_diff[start-1])
