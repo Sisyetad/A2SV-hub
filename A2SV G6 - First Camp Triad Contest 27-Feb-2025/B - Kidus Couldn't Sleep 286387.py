b56844e76683acbd032bc4248525817b48754710
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-02-27 22:04:36 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/B - Kidus Couldn't Sleep 286387.py
@@ -0,0 +1,13 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+window = 0
+for i in range(k):
+    window += arr[i]
+total = window
+for right in range(k,n):
+    window += arr[right]
+    window -= arr[right-k]
+    total += window
+print(f"{total/(n-k+1):.6f}")
