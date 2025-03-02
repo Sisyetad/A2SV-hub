7b593ae087e5c20fe9940dd8c1c61d4781b9bb22
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-02 10:45:15 +0300

    Created 'I - The Odd Challenge' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/I - The Odd Challenge 287994.py
@@ -0,0 +1,19 @@
+# Problem: I - The Odd Challenge - https://codeforces.com/gym/589822/problem/I
+
+t = int(input())
+for j in range(t):
+    n, m = map(int, input().split())
+    prefix_sum = list(map(int, input().split()))
+    
+    prefix_sum.insert(0,0)
+    for i in range(1,n+1):
+        prefix_sum[i] += prefix_sum[i-1]
+    
+    for _ in range(m):
+        l, r, k = map(int, input().split())
+        total_sum = prefix_sum[-1] - (prefix_sum[r] - prefix_sum[l-1]) + ((r-l+1)*k)
+        if total_sum%2:
+            print("YES")
+        else:
+            print("NO")
+    
