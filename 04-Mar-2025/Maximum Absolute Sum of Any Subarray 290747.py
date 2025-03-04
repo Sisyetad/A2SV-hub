6d21900b8f4cedd4546a6922a3f2e88079ba9446
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-04 11:48:07 +0300

    Created 'Maximum Absolute Sum of Any Subarray' by A2SV Hub

---
+++ 04-Mar-2025/Maximum Absolute Sum of Any Subarray 290747.py
@@ -0,0 +1,16 @@
+# Problem: Maximum Absolute Sum of Any Subarray - https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/
+
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        prefix = 0
+        ans = 0
+        n = len(nums)
+        mins = maxs = 0
+        
+        for i in range(n):
+            prefix += nums[i]
+            maxs = max(maxs, prefix)
+            mins = min(mins, prefix)
+            
+        ans = maxs - mins
+        return ans
