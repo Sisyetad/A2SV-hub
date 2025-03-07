87c47ed4c5fded2d7a0d54c792d17b18f6262109
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-08 00:00:35 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 07-Mar-2025/Red and Blue 297643.py
@@ -0,0 +1,17 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
+    maxs1 = maxs2 = 0
+    prefix1 = prefix2 = 0
+    for i in range(n):
+        prefix1 += a[i]
+        maxs1 = max(maxs1, prefix1)
+    for j in range(m):
+        prefix2 += b[j]
+        maxs2 = max(maxs2, prefix2)
+    print(maxs1 + maxs2)
