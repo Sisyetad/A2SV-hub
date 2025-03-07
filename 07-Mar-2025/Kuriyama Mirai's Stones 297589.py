2194448a06a3b5995e12790d2aba1b35a0e08d37
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-07 23:21:04 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 07-Mar-2025/Kuriyama Mirai's Stones 297589.py
@@ -0,0 +1,19 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr1 = arr[:]
+arr.sort()
+arr.insert(0,0)
+arr1.insert(0,0)
+for i in range(1,n+1):
+    arr1[i] += arr1[i-1]
+    arr[i] += arr[i-1]
+m = int(input())
+
+for i in range(m):
+    t, l, r = map(int, input().split())
+    if t == 1:
+        print(arr1[r] - arr1[l-1])
+    else:
+        print(arr[r] - arr[l-1])
