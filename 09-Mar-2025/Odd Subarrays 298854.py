8d42c3f9c39cd6280218438e26c72e83f2f7514a
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-09 21:28:58 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 09-Mar-2025/Odd Subarrays 298854.py
@@ -0,0 +1,17 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    mono_stack = []
+    ans = 0
+    for i in range(n):
+        if mono_stack and mono_stack[-1] < arr[i]:
+            mono_stack.pop()
+            
+        mono_stack.append(arr[i])
+        if len(mono_stack) == 2:
+            ans += 1
+            mono_stack = []
+    print(ans)
