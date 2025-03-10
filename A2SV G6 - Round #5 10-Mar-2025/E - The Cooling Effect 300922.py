3a14d7498ba217357a05496c23b02a4378902688
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-11 00:09:02 +0300

    Created 'E - The Cooling Effect' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/E - The Cooling Effect 300922.py
@@ -0,0 +1,18 @@
+# Problem: E - The Cooling Effect - https://codeforces.com/gym/591928/problem/E
+
+# Multiple test cases
+t = int(input())
+for _ in range(t):
+    input()
+    n, k = map(int,input().split())
+    arr = list(map(int, input().split()))
+    temp = list(map(int, input().split()))
+    
+    result = [float("inf")] * n
+    for i in range(k):
+        result[arr[i]-1] = temp[i]
+    for i in range(1,n):
+        result[i] = min(result[i], result[i-1] + 1)
+    for i in range(2,n+1):
+        result[-i] = min(result[-i], result[-i+1]+1)
+    print(*result)
