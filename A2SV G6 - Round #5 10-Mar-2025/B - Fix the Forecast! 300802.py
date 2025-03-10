57df50e86dce0e1b80719c445281bf6ebbf3446d
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-10 22:47:42 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/B - Fix the Forecast! 300802.py
@@ -0,0 +1,13 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    indexed_a = sorted(enumerate(a), key=lambda x: x[1])
+    b.sort()
+    result = [0] * n
+    for (index, _), b_value in zip(indexed_a, b):
+        result[index] = b_value
+    print(*result)
