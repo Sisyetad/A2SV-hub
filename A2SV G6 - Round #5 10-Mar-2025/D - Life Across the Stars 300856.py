ac7d9165f20293653c731964ee22270d19c373ba
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-10 23:19:05 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/D - Life Across the Stars 300856.py
@@ -0,0 +1,19 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+n = int(input())
+prefix = defaultdict(int)
+for _ in range(n):
+    a, b = map(int, input().split())
+    prefix[a] += 1
+    prefix[b] -= 1
+
+max_alive = 0
+current = 0
+best = float("inf")
+for i in sorted(prefix):
+    current += prefix[i]
+    if current > max_alive:
+        max_alive = current
+        best = i
+print(best, max_alive)
