7a3e574c30f9853bafa8ad4308f4df17e5fa1378
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-10 20:59:05 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/C - Escape-Proof Transfers 300641.py
@@ -0,0 +1,16 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n, t, c = map(int, input().split())
+arr = list(map(int, input().split()))
+count = ans = 0
+right = 0
+while right < n:
+    if  arr[right] <= t:
+        count += 1
+    else:
+        count = 0
+    
+    if count >= c:
+        ans += 1
+    right += 1
+print(ans)
