7c61032341efc4b985d758def916db7232b43834
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-11 21:11:57 +0300

    Created 'Power of Four' by A2SV Hub

---
+++ 11-Mar-2025/Power of Four 303092.py
@@ -0,0 +1,10 @@
+# Problem: Power of Four - https://leetcode.com/problems/power-of-four/
+
+class Solution:
+    def isPowerOfFour(self, n) :
+        if n == 1:
+            return True
+        if n%4 != 0 or n == 0:
+            return False
+            
+        return self.isPowerOfFour(n//4)
