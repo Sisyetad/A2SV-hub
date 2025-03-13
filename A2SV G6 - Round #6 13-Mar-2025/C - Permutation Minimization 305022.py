b95cd3b489d6f5e6cd10d7b0c6bb4378d4ce0509
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-13 17:28:43 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 13-Mar-2025/C - Permutation Minimization 305022.py
@@ -0,0 +1,18 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+from collections import deque
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    stack = []
+    ans = deque()
+    for i in range(n):
+        while stack and stack[-1] > arr[i]:
+            stack.pop()
+        stack.append(arr[i])
+        if stack[0] >= arr[i]:
+            ans.appendleft(arr[i])
+        else:
+            ans.append(arr[i])
+    print(*ans)
