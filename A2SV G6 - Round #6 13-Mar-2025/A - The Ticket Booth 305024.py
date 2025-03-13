9488f4d389d667d98172d7e998c4395b2343f85f
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-13 17:28:52 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 13-Mar-2025/A - The Ticket Booth 305024.py
@@ -0,0 +1,7 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+n, s = map(int, input().split())
+if s//n < s/n:
+    print(s//n+1)
+elif s//n == s/n:
+    print(s//n)
