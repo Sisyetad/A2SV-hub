33213e92bc479d7ea7cefd74617d8c0b0e642b48
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-13 17:28:43 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 13-Mar-2025/B - Nafargi's Binary Reduction Game 305021.py
@@ -0,0 +1,17 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+n = int(input())
+s = input()
+stack = []
+for i in range(n):
+    if s[i] == '0':
+        if stack and stack[-1] == '1':
+            stack.pop()
+        else:
+            stack.append(s[i])
+    else:
+        if stack and stack[-1] == '0':
+            stack.pop()
+        else:
+            stack.append(s[i])
+print(len(stack))
