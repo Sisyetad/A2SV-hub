6c833929a38648223ff28410c6e7ee7b0636777e
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-13 17:28:35 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 13-Mar-2025/D - Meklit's Chat Screen 305019.py
@@ -0,0 +1,19 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+ans = deque()
+sets = set()
+for i in range(n):
+    if len(ans) >= k:
+        if arr[i] not in sets:
+            top = ans.pop()
+            sets.remove(top)
+            ans.appendleft(arr[i])
+            sets.add(arr[i])
+    elif arr[i] not in sets:
+        ans.appendleft(arr[i])
+        sets.add(arr[i])
+print(len(ans))
+print(*ans)
