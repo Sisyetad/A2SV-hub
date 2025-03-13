c60c2ad7b74bf6b85dd89d8c01432f766b45503a
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-13 17:28:26 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 13-Mar-2025/E - Minimum Subsequence 305017.py
@@ -0,0 +1,33 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+t = int(input())    
+for _ in range(t):
+    n = int(input())
+    s = input()
+    
+    # Stacks to store subsequences
+    zero_stack = []
+    one_stack = []
+    ans = [0] * n
+    seq_count = 0
+    
+    for i in range(n):
+        if s[i] == '0':
+            if one_stack:
+                seq_id = one_stack.pop()
+            else:
+                seq_count += 1
+                seq_id = seq_count
+            zero_stack.append(seq_id)
+        else:  # s[i] == '1'
+            if zero_stack:
+                seq_id = zero_stack.pop()
+            else:
+                seq_count += 1
+                seq_id = seq_count
+            one_stack.append(seq_id)
+        
+        ans[i] = seq_id
+    
+    print(seq_count)
+    print(*ans)
