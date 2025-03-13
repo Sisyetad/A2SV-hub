49d0aa6aaef72f37e79dfeb62b29c1a9fba1ab4b
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-13 17:28:17 +0300

    Created 'F - Nahom's Array Dilemma' by A2SV Hub

---
+++ A2SV G6 - Round #6 13-Mar-2025/F - Nahom's Array Dilemma 305016.py
@@ -0,0 +1,26 @@
+# Problem: F - Nahom's Array Dilemma - https://codeforces.com/gym/594077/problem/F
+
+# Multiple test cases
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    def solve(n, arr):
+        pref = 0
+        mono_stack = []
+        for i in range(n):
+            while mono_stack and mono_stack[-1][0] <= arr[i]:
+                top = mono_stack.pop()
+                if (pref - top[-1]) > 0:
+                    return False
+            mono_stack.append((arr[i],pref))
+            pref += arr[i]
+        return True
+    
+    ans1 = solve(n,arr)
+    ans2 = solve(n,arr[::-1])
+    if ans1 and ans2:
+        print("YES")
+    else:
+        print("NO")
