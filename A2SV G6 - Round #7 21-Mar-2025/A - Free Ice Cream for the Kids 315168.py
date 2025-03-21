c9689816095639f5cd1eed17b039ddbb10983d3f
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-21 21:37:00 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 21-Mar-2025/A - Free Ice Cream for the Kids 315168.py
@@ -0,0 +1,14 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+n, x = map(int, input().split())
+dis = 0
+for _ in range(n):
+    oper, val = input().split()
+    if oper == '-':
+        if int(val) <= x:
+            x -= int(val)
+        else:
+            dis += 1
+    else:
+        x += int(val)
+print(x, dis)
