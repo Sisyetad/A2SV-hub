81c045a38656da56b74f95d233e2590acab5d120
Sisay Tadewos <160520835+Sisyetad@users.noreply.github.com>
2025-03-23 20:26:31 +0300

    Created 'E - Strange Mirroring' by A2SV Hub

---
+++ A2SV G6 - Round #7 23-Mar-2025/E - Strange Mirroring 316189.py
@@ -0,0 +1,28 @@
+# Problem: E - Strange Mirroring - https://codeforces.com/gym/596141/problem/E
+
+def find_kth_character(s, k):
+    n = len(s)
+    flipped = False  
+    i = 0  
+
+    while (2**i) * n < k:
+        i += 1
+
+    while k > n:
+        half = (2**(i - 1)) * n
+        if k > half:
+            k -= half  
+            flipped = not flipped 
+        i -= 1
+        
+    return s[k-1].swapcase() if flipped else s[k-1]
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    q = int(input())
+    queries = map(int, input().split())
+    result = []
+    for k in queries:
+        result.append(find_kth_character(s, k))
+    print(*result)
